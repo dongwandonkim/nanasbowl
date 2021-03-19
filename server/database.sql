32373584155d2d635985b7b52a042be49bb5b5a2
Dongwan Kim <dongwan.don.kim@gmail.com>
2021-03-18 22:57:31 -0700

    dbsql for search options

--- server/database.sql
+++ server/database.sql
@@ -63,5 +63,9 @@ CREATE TABLE product (
    pet_type_id            BIGINT NOT NULL REFERENCES pet_type(id)
 );
 
-//search options
+// #1 search keyword
 SELECT * FROM ingredient WHERE name LIKE '%keyword%';
+//
+SELECT product_id FROM ingredient_product WHERE ingredient_id = value;
+//
+SELECT product.id, product.name, product_type.type, product.created_at FROM product, product_type WHERE product.id = value AND product.product_type_id = product_type.id;
