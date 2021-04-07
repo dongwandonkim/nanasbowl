e401d96f7534bb634ecbd35112704944dc39b59a
Dongwan Kim <dongwan.don.kim@gmail.com>
2021-04-07 06:39:15 -0700

    testing hard coded env

--- server/db.js
+++ server/db.js
@@ -4,11 +4,11 @@ require('dotenv').config();
 console.log(process.env.PGUSER);
 
 const pool = new Pool({
-  user: process.env.PGUSER,
+  user: 'ubuntu',
   password: 'ehddhks1',
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  database: process.env.PGDATABASE,
+  host: 'localhost',
+  port: '5432',
+  database: 'nanasbowl',
 });
 
 module.exports = pool;
